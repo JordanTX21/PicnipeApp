731fd85d9eb3a64de3b105f5ddbfe6245a35a7a6
JordanTX21 <jordantx21@gmail.com>
2022-05-27 01:44:45 -0500

    Inicio de proyecto

---
+++ app/src/main/java/com/example/picnipeappp/ui/search/SearchFragment.kt
@@ -0,0 +1,45 @@
+package com.example.picnipeappp.ui.search
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.picnipeappp.R
+import androidx.lifecycle.Observer
+import com.example.picnipeappp.databinding.FragmentSearchBinding
+import com.example.picnipeappp.ui.search.SearchViewModel
+
+class SearchFragment : Fragment() {
+    private lateinit var searchViewModel: SearchViewModel
+    private var _binding: FragmentSearchBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        searchViewModel =
+            ViewModelProvider(this).get(SearchViewModel::class.java)
+
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSearch
+        searchViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
